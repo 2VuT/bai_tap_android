276dbc00b22caa268ddc19b3c6eae25065f1d24d
HaiPTIT <haivtndc@gmail.com>
2020-08-22 17:16:16 +0700

    amend listview

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.location_list);
+        String[] locations = new String[] {
+                "Ho Tay",
+                "Lang Bac",
+                "Thap Rua",
+                "Chua Mot Cot",
+                "Quoc Tu Giam",
+                "Thu Vien QG"
+        };
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, locations);
+        listView.setAdapter(arrayAdapter);
     }
 }
